34cb817c9be0f50c6183bbe5bc9fc5a511c8f49e
wangchunpeng <jaxwang319@qq.com>
2024-03-28 20:00:58 +0800

    sh

--- restart.sh
+++ restart.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+ps aux | grep gunicorn
+
+echo "reload now..."
 pkill -HUP -f gunicorn
+
+ps aux | grep gunicorn
--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+ps aux | grep gunicorn
+
+echo "kill gunicorn now..."
+
 pkill gunicorn
-gunicorn -w 5 -b 127.0.0.1:5001 -D run:app
+
+gunicorn -w 4 -b 127.0.0.1:5001 -D run:app
+
+ps aux | grep gunicorn
